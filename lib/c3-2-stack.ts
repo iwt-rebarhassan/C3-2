242621123ebdebe9811c40d277388da3996b02a3
Rebar Hassan <rebar.hassan@immowelt.de>
2020-10-27 22:09:05 +0100

    Initial commit.

--- lib/c3-2-stack.ts
+++ lib/c3-2-stack.ts
@@ -1,18 +1,31 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as dynamoDB from '@aws-cdk/aws-dynamodb';
+import * as apiGatewayV2 from '@aws-cdk/aws-apigatewayv2';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class C32Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'C32Queue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const lambdaFunction = new lambda.Function(this, `${this.stackName}-lambdaFunction`,{
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'function.handler'
+    })
 
-    const topic = new sns.Topic(this, 'C32Topic');
+    const functionIntegration = new apiGatewayV2.LambdaProxyIntegration({
+      handler: lambdaFunction
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const httpApi = new apiGatewayV2.HttpApi(this, `${this.stackName}-httpApi`, {
+      apiName: `${this.stackName}-httpApi`,
+      defaultIntegration: functionIntegration
+    })
+
+    const dynamoDBTable = new dynamoDB.Table(this, `${this.stackName}-dynamoDB-table`, {
+      partitionKey: {name: 'key', type: dynamoDB.AttributeType.STRING}
+    })
+
+    lambdaFunction.addEnvironment('tableName', dynamoDBTable.tableName)
   }
 }
